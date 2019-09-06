e55adde64c8edc34e299105619bd587675209cd8
Nicholas Interest <NicholasInterest1@users.noreply.github.com>
2019-09-06 16:57:43 -0400

    final additions

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDarkMode } from "../hooks/useDarkMode";
+import styled from "styled-components";
 
 function NavBar() {
   const [darkMode, setDarkMode] = useDarkMode(false);
@@ -12,9 +13,13 @@ function NavBar() {
   return (
     <div className="navbar">
       <h1>Women's World Cup</h1>
-        <button data-testid="darkmode" onClick={toggleMode}>Dark Mode: {darkMode ? 'On': 'Off'}</button>
+        <TheBtn data-testid="darkmode" onClick={toggleMode}>Dark Mode: {darkMode ? 'On': 'Off'}</TheBtn>
       </div>
   );
 };
+
+const TheBtn = styled.button`
+  border-radius: 4px;
+`;
 
 export default NavBar;
--- client/src/components/PlayersCard.js
+++ client/src/components/PlayersCard.js
@@ -3,13 +3,18 @@ import styled from "styled-components";
 
 function PlayersCard(props) {
   return (
-    <div className="player-wrapper">
       <div className="player-info">
+          <TheA
+        href={`http://www.wikipedia.org/wiki/${props.data.name
+          .split(' ')
+          .join(' ')}`}
+        target='_blank'
+        rel='noopener noreferrer'>
         <TheP>Name: {props.data.name}</TheP>
         <TheP2>Country: {props.data.country}</TheP2>
         <TheP3>Google Searches: {props.data.searches}</TheP3>
+        </TheA>
       </div>
-    </div>
   );
 }
 
@@ -25,6 +30,10 @@ const TheP2 = styled.p`
 const TheP3 = styled.p`
     color: blue;
     padding: 15px;
+`;
+
+const TheA = styled.a`
+    text-decoration: none;
 `;
 
 export default PlayersCard;
