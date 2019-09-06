119439a5dac360fa203ced1afb92e9f16620e814
Nicholas Interest <NicholasInterest1@users.noreply.github.com>
2019-09-06 16:23:54 -0400

    A bit more styling

--- client/src/components/PlayersCard.js
+++ client/src/components/PlayersCard.js
@@ -1,15 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 function PlayersCard(props) {
   return (
     <div className="player-wrapper">
       <div className="player-info">
-        <p>Name: {props.data.name}</p>
-        <p>Country: {props.data.country}</p>
-        <p>Google Searches: {props.data.searches}</p>
+        <TheP>Name: {props.data.name}</TheP>
+        <TheP2>Country: {props.data.country}</TheP2>
+        <TheP3>Google Searches: {props.data.searches}</TheP3>
       </div>
     </div>
   );
 }
+
+const TheP = styled.p`
+    color: red;
+    margin-top: 10px;
+    padding: 15px;
+`;
+const TheP2 = styled.p`
+    color: white;
+    padding: 15px;
+`;
+const TheP3 = styled.p`
+    color: blue;
+    padding: 15px;
+`;
 
 export default PlayersCard;
